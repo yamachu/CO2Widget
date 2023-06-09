736800aa325bdba482d37f2d91fbc231edb548e1
yamachu <yamachu.dev@gmail.com>
2023-06-10 01:52:31 +0900

    Bye mobile and tizen

--- src/CO2Widget/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace CO2Widget;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
